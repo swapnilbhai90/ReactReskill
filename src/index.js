80d851c03f44f2fe4ce51616664b5ed6a353a85e
Swapnil Bhaisare <swapnilbhai90@gmail.com>
2024-11-13 10:32:42 +0530

    Api call

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Example from './tailwind/TailWindCard';
-import { ThemeProvider } from "@material-tailwind/react";
+import Example, { SimpleCard } from './tailwind/TailWindCard';
+import { MaterialTailwindTheme, ThemeProvider } from "@material-tailwind/react";
 import Gallery from './ui/profile';
+import ApiCall from './apicall/ApiCall';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  <Gallery/>
+<React.StrictMode>
+<ApiCall/>
   </React.StrictMode>
 );
 
